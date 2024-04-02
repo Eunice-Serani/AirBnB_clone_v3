7fda19de598d48d4e21b6f895f002ce5e04a274d
Alfred Nti <87778148+Freddywhest@users.noreply.github.com>
2024-04-02 03:54:19 -0700

    Updated Task-14

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -16,13 +17,23 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        places = relationship("Place", backref="user",
+                              cascade="all, delete")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
+
+    def __setattr__(self, name, value):
+        """set attributes, use a hash to encrypt the password"""
+        if name == "password":
+            hash = md5(value.encode('utf-8'))
+            self.__dict__[name] = hash.hexdigest()
+        else:
+            self.__dict__[name] = value
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
